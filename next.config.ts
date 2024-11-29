ae249b04530095eec39a42b2d17f947e65792f6e
ujj-devv <164148333+Abhaytnv112@users.noreply.github.com>
2024-11-29 17:41:16 +0530

    WIP:initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  sassOptions: {
+    implementation: 'sass-embedded',
+  },
+}
+ 
+export default nextConfig
